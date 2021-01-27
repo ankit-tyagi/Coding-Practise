58ab3a9de63986ec86a65a2e140d43b5c1d98ac4
Ankit Tyagi <ankittyaagi@gmail.com>
2021-01-28 01:26:08 +0530

    week 04 date 01.22

---
+++ LeetCode/2021/Week 04/week 04 date 01.22 - Determine if Two Strings Are Close.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int n = 26;
+
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
+
+        for(char ch : word1.toCharArray()){
+            arr1[ch - 'a']++;
+        }
+        for(char ch : word2.toCharArray()){
+            arr2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] == arr2[i]) {
+                continue;
+            }
+            if (arr1[i] == 0 || arr2[i] == 0) {
+                return false;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
