8e492d40f3ffb1baea1f78d318062d34e346f8fd
Ankit Tyagi <ankittyaagi@gmail.com>
2021-01-28 00:48:00 +0530

    2021/Week 04/week 04 date 01/23

--- LeetCode/2021/Week 04/week 04 date 01/23 - Sort the Matrix Diagonally.java
+++ LeetCode/2021/Week 04/week 04 date 01/23 - Sort the Matrix Diagonally.java
@@ -1,3 +1,36 @@
-public class 23 - Sort the Matrix Diagonally {
+class Solution {
     
+    //First Solution
+    public int[][] diagonalSort(int[][] mat) {
+        
+        for(int i=0, j=0; i<mat.length ;i++){
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int k=0; (i+k)<mat.length && (j+k)<mat[i].length;k++){
+                arr.add(mat[i+k][j+k]);
+            }
+            arr = (ArrayList<Integer>) sortTheArray(arr);
+            for(int k=0; (i+k)<mat.length && (j+k)<mat[i].length;k++){
+                mat[i+k][j+k] = arr.get(k);
+            }
+        }
+
+        for(int i=0, j=1; j<mat[i].length ;j++){
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int k=0; (i+k)<mat.length && (j+k)<mat[i].length;k++){
+                arr.add(mat[i+k][j+k]);
+            }
+            arr = (ArrayList<Integer>) sortTheArray(arr);
+            for(int k=0; (i+k)<mat.length && (j+k)<mat[i].length;k++){
+                mat[i+k][j+k] = arr.get(k);
+            }
+        }
+
+        return mat;
+    }
+
+    public static List<Integer> sortTheArray(ArrayList<Integer> arr){
+        Collections.sort(arr);
+        return arr;
+//        return arr.stream().sorted().collect(Collectors.toList());
+    }
 }
