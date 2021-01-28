d790f067487281f03b9d41e367a9383b4990635f
Ankit Tyagi <ankittyaagi@gmail.com>
2021-01-29 02:47:12 +0530

    week 04 date 01.24

--- LeetCode/2021/Week 04/week 04 date 01.24 - Merge k Sorted Lists.java
+++ LeetCode/2021/Week 04/week 04 date 01.24 - Merge k Sorted Lists.java
@@ -16,7 +16,34 @@ class Solution {
         }
         return resultNode;
     }
-    
+
+    public ListNode mergeKListsSecondSolution(ListNode[] lists) {
+        ListNode result = null, last = null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparing(i -> i.val));
+        for (ListNode list : lists) {
+            if(list != null) {
+                pq.add(list);
+            }
+        }
+        if(pq.size()<1)
+            return result;
+        
+        while (!pq.isEmpty()){
+            ListNode temp = pq.poll();
+            if(result == null){
+                result = last = temp;
+            }else{
+                last.next = temp;
+                last = temp;
+                
+            }
+            if(temp.next != null){
+                pq.add(temp.next);
+            }
+        }
+        return result;
+    }
+   
     private ListNode calculateResult(ListNode resultNode, ListNode list) {
         if(list == null)
             return resultNode;
